6969b4191f9d668c8dbe8bfa90648767b6facb94
Filippo Di Marco <filidm5795@gmail.com>
2022-12-22 19:06:26 +0100

    created form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [data, setData] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleForm = (event : any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setData(data => ({
+      ...data,
+      [name] : value
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>username</label>
+        <input onChange={handleForm} type="text" name="username" value={data.username}/>
+        <label>password</label>
+        <input onChange={handleForm} type="text" name="password" value={data.password}/>
+        <button type="button" onClick={()=>{
+          console.log(data);
+        }}>Login</button>
+      </form>
     </div>
   );
 }
